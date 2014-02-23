8fd5159921f130240733f8e3b486eeee6588e9e4
Shailen Tuli <shailen.tuli@gmail.com>
2014-02-23 14:38:23 -0800

    Tests for dom elements

--- web/3-buttonbadge/piratebadge_test.dart
+++ web/3-buttonbadge/piratebadge_test.dart
@@ -1,27 +1,81 @@
-library piratebadge_test;
+library piratebadgetb.est;
 
 import 'package:unittest/unittest.dart';
 import 'package:di/di.dart';
 import 'package:angular/angular.dart';
 import 'package:angular/mock/module.dart';
 import 'piratebadge.dart' show PirateModule, BadgesController;
+import 'dart:html' show querySelector;
 
 main() {
   setUp(setUpInjector);
   tearDown(tearDownInjector);
 
   group('pirate badge', () {
-    setUp(module((Module m) => m.install(new PirateModule())));
+    TestBed tb;
+    var name = 'misko';
+
+    // Get this from lib.
+    var badgeName = 'Anne Bonney';
+
+    setUp(module((Module m) {
+      m.install(new PirateModule());
+      inject((TestBed testBed) => tb = testBed);
+      inject((BadgesController _ctrl) => tb.rootScope.ctrl = _ctrl);
+    }));
 
     test('should load recipes', inject((BadgesController ctrl) {
       expect(ctrl.inputIsNotEmpty, isFalse);
       expect(ctrl.label, 'Aye! Gimme a name!');
     }));
 
-    test('generating name', inject((BadgesController ctrl) {
+    test('should generate name', inject((BadgesController ctrl) {
       expect(ctrl.name, isEmpty);
       ctrl.generateName();
       expect(ctrl.name, 'Anne Bonney');
     }));
+
+    test('should bind model to input', () {
+      tb.compile('''<div><span id="badgeName">{{ctrl.name}}</span></div>''');
+      var span = tb.rootElement.querySelector('#badgeName');
+      expect(span.innerHtml, isEmpty);
+      tb.rootScope.ctrl.name = name;
+      tb.rootScope.$digest();
+      expect(span.innerHtml, name);
+    });
+
+    test('should update badge from input value', () {
+      tb.compile('''<div><input type="text" id="inputName" maxlength="15"
+          ng-model="ctrl.name" probe="p">
+          <span id="badgeName">{{ctrl.name}}</span>
+      </div>''');
+      Probe probe = tb.rootScope.p;
+      var ngModel = probe.directive(NgModel);
+
+      var input = probe.element;
+      var span = tb.rootElement.querySelector('#badgeName');
+      expect(input.value, isEmpty);
+      expect(span.innerHtml, isEmpty);
+
+      input.value = name;
+      tb.triggerEvent(input, 'change');
+
+      expect(tb.rootScope.ctrl.name, name);
+      expect(span.innerHtml, name);
+    });
+
+    test('should generate static name when button is pressed', () {
+      tb.compile('''<div><button ng-click="ctrl.generateName()"
+          ng-disabled="ctrl.inputIsNotEmpty" probe="p">{{ctrl.label}}</button>
+          <span id="badgeName">{{ctrl.name}}</span>
+        </div>''');
+      Probe probe = tb.rootScope.p;
+      var ngModel = probe.directive(NgModel);
+      var button = tb.rootElement.querySelector('button');
+      var span = tb.rootElement.querySelector('#badgeName');
+      button.click();
+      tb.rootScope.$digest();
+      expect(span.innerHtml, 'Anne Bonney');
+    });
   });
 }
